b3c83a6fddca3d6f8e2896d23ad99c3f33c9c35f
Valentin Heinisch <valentin.heinisch@gmx.de>
2023-04-01 21:52:15 +0200

    rustfmt changes

--- src/formatter.rs
+++ src/formatter.rs
@@ -1,21 +1,18 @@
 use std::char::from_digit;
+use std::fmt;
 use std::i64;
 use std::num::ParseIntError;
-use std::fmt;
 
 #[derive(Debug, Clone)]
 pub struct BaseError;
 
-impl fmt::Display for BaseError
-{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result
-    {
-            write!(f, "The given base could not be calculated.")
+impl fmt::Display for BaseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "The given base could not be calculated.")
     }
 }
 
-pub fn format_output(num: u64, base: u32) -> Result<String, BaseError>
-{
+pub fn format_output(num: u64, base: u32) -> Result<String, BaseError> {
     if base < 2 || base > 36 {
         return Err(BaseError);
     }
@@ -29,17 +26,16 @@ pub fn format_output(num: u64, base: u32) -> Result<String, BaseError>
         let div = num as f64 / base.pow(max_pow) as f64;
         if div >= 1.0 {
             max_pow += 1;
-        }
-        else {
+        } else {
             max_pow -= 1;
             break;
         }
     }
-    
+
     let mut res = Vec::new();
     let mut remainder = num;
     for i in (0..=max_pow).rev() {
-        res.push(remainder/base.pow(i) as u64);
+        res.push(remainder / base.pow(i) as u64);
         remainder = remainder % base.pow(i) as u64;
     }
 
@@ -51,28 +47,22 @@ pub fn format_output(num: u64, base: u32) -> Result<String, BaseError>
     Ok(result_string)
 }
 
-pub fn parse_number(input: &String, base: u32) -> Result<i64, ParseIntError>
-{
+pub fn parse_number(input: &String, base: u32) -> Result<i64, ParseIntError> {
     let input_no_prefix = input.trim_start_matches("0b").trim_start_matches("0x");
-    match i64::from_str_radix(input_no_prefix, base)
-    {
+    match i64::from_str_radix(input_no_prefix, base) {
         Ok(parsed_int) => return Ok(parsed_int),
-        Err(e) => return Err(e)
+        Err(e) => return Err(e),
     };
 }
 
-pub fn deduct_base(input: &String) -> u32
-{
+pub fn deduct_base(input: &String) -> u32 {
     if input.starts_with("0b") {
         return 2;
-    }
-    else if input.starts_with("0o") {
+    } else if input.starts_with("0o") {
         return 8;
-    }
-    else if input.starts_with("0x") {
+    } else if input.starts_with("0x") {
         return 16;
-    }
-    else {
+    } else {
         return 10;
     }
 }
@@ -83,28 +73,28 @@ mod tests {
 
     #[test]
     fn test_format_output() {
-        let mut in_num : u64 = 10;
-        let mut in_base : u32 = 2;
-        let out_string : String = format_output(in_num, in_base).expect("");
+        let mut in_num: u64 = 10;
+        let mut in_base: u32 = 2;
+        let out_string: String = format_output(in_num, in_base).expect("");
         assert_eq!(out_string, "1010".to_string());
 
         in_num = 175;
         in_base = 16;
-        let out_string : String = format_output(in_num, in_base).expect("");
+        let out_string: String = format_output(in_num, in_base).expect("");
         assert_eq!(out_string, "af".to_string());
 
         in_num = 255;
         in_base = 10;
-        let out_string : String = format_output(in_num, in_base).expect("");
+        let out_string: String = format_output(in_num, in_base).expect("");
         assert_eq!(out_string, "255".to_string());
     }
 
     #[test]
     #[should_panic]
     fn test_format_output_panic() {
-        let mut in_num : u64 = 10;
-        let mut in_base : u32 = 1;
-        let mut _out_string : String = format_output(in_num, in_base).expect("");
+        let mut in_num: u64 = 10;
+        let mut in_base: u32 = 1;
+        let mut _out_string: String = format_output(in_num, in_base).expect("");
 
         in_num = 175;
         in_base = 37;
@@ -113,9 +103,9 @@ mod tests {
 
     #[test]
     fn test_parse_number() {
-        let mut in_string : String = "465".to_string();
-        let mut in_base : u32 = 10;
-        let mut out_num : i64 = parse_number(&in_string, in_base).expect("");
+        let mut in_string: String = "465".to_string();
+        let mut in_base: u32 = 10;
+        let mut out_num: i64 = parse_number(&in_string, in_base).expect("");
         assert_eq!(out_num, 465);
 
         in_string = "11111111".to_string();
@@ -141,8 +131,8 @@ mod tests {
 
     #[test]
     fn test_deduct_base() {
-        let mut in_string : String = "0b101".to_string();
-        let mut out_base : u32 = deduct_base(&in_string);
+        let mut in_string: String = "0b101".to_string();
+        let mut out_base: u32 = deduct_base(&in_string);
         assert_eq!(out_base, 2);
 
         in_string = "0o255".to_string();
@@ -157,5 +147,4 @@ mod tests {
         out_base = deduct_base(&in_string);
         assert_eq!(out_base, 16);
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,32 @@
 mod formatter;
 
-use formatter::{ deduct_base, format_output, parse_number };
 use clap::Parser;
+use formatter::{deduct_base, format_output, parse_number};
 
 #[derive(Parser)]
 #[clap(version, about, long_about = None)]
 /// A simple base conversion tool
-struct CliInput
-{
+struct CliInput {
     /// The input number
     input_number: String,
     /// The output base
     output_base: u32,
     /// The input base. If omitted, the base is tried to be deducted
     /// Looks if the input is starting with 0b or 0x and otherwise tries to use decimal base
-    input_base: Option<u32>
+    input_base: Option<u32>,
 }
 
-fn main()
-{
+fn main() {
     let args = CliInput::parse();
 
     let base_in = match args.input_base {
         Some(b) => b,
-        None    => deduct_base(&args.input_number)
+        None => deduct_base(&args.input_number),
     };
 
-    let parsed_int = match parse_number(&args.input_number, base_in)
-    {
+    let parsed_int = match parse_number(&args.input_number, base_in) {
         Ok(parsed_int) => parsed_int,
-        Err(_e) =>
-        {
+        Err(_e) => {
             println!("Invalid usage! Can't parse given input number.");
             return;
         }
@@ -39,4 +35,3 @@ fn main()
     let output = format_output(parsed_int as u64, args.output_base).expect("Error aua!");
     println!("{}", output);
 }
-
